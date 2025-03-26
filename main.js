f679f3271c4ac59b3f3159a631f41b8299a917a7
Hugo Perez <hugopereztm12309@gmail.com>
2025-03-26 17:06:28 -0400

    Create Gameboard IIFE and declare the 3x3 grid

---
+++ main.js
@@ -0,0 +1,13 @@
+const Gameboard = (function() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    //3 x 3 game board
+    for(let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+})();
